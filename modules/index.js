ca8476095b9b99b087680e3875859f8566b8bd20
Rami B <ramibasheer9@gmail.com>
2022-09-28 21:08:48 +0200

    apply solution

---
+++ modules/index.js
@@ -0,0 +1,6 @@
+import { scrollWindow } from "./scroll.js";
+import { hoverHeader } from "./hover.js";
+import { clickImages } from "./click.js";
+console.log(scrollWindow);
+console.log(hoverHeader);
+console.log(clickImages);
